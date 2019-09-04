260cf5edd88b2d54f96440cf4c62bfc8f8c21a3b
Muslim Guseinov <gusejnov1999@list.ru>
2019-09-05 00:36:20 +0300

    refactor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,7 +12,16 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let pool = ThreadPool::new(4).unwrap();
 
+    let mut counter = 0;
+
     for stream in listener.incoming() {
+        if counter == 2 {
+            println!("Shutting down");
+            break;
+        }
+
+        counter += 1;
+
         let stream = stream.unwrap();
 
         pool.execute(|| {
@@ -44,6 +53,5 @@ fn handle_connection(mut stream: TcpStream) {
     let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
-
     stream.flush().unwrap();
 }
