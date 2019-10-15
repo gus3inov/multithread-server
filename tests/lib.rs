e30730dcc9f586b98fdb34519430a1f294500fc2
Muslim Guseinov <gusejnov1999@list.ru>
2019-10-15 20:09:44 +0300

    test(lib)

--- tests/lib.rs
+++ tests/lib.rs
@@ -6,64 +6,53 @@ use std::time::Duration;
 use threadpool::ThreadPool;
 
 #[test]
-pub fn thread_pool_size_zero() {
-    let pool = ThreadPool::new(0);
-    match pool {
-        Ok(_) => assert!(false, "pool should not create if size zero"),
-        Err(_) => assert!(true),
-    }
-}
-
-#[test]
 fn one_thread() {
-    if let Ok(pool) = ThreadPool::new(1) {
-        let (tx, rx) = mpsc::sync_channel(0);
+    let (sender, _) = ThreadPool::fixed_size(1);
+    let (tx, rx) = mpsc::sync_channel(0);
 
-        pool.execute(move || {
+    sender
+        .send(move || {
             tx.send("lol").unwrap();
-        });
+        })
+        .unwrap();
 
-        assert_eq!("lol", rx.recv().unwrap());
-    } else {
-        assert!(false, "pool should be create")
-    }
+    assert_eq!("lol", rx.recv().unwrap());
 }
 
 #[test]
 fn two_thread() {
-    if let Ok(pool) = ThreadPool::new(2) {
-        let (tx, rx) = mpsc::sync_channel(0);
+    let (sender, _) = ThreadPool::fixed_size(2);
+    let (tx, rx) = mpsc::sync_channel(0);
 
-        for _ in 0..2 {
-            let tx = tx.clone();
-            pool.execute(move || {
+    for _ in 0..2 {
+        let tx = tx.clone();
+        sender
+            .send(move || {
                 tx.send("lol").unwrap();
                 thread::sleep(Duration::from_millis(500));
 
                 tx.send("kek").unwrap();
                 thread::sleep(Duration::from_millis(500));
-            });
-        }
+            })
+            .unwrap();
+    }
 
-        for &msg in ["lol", "lol", "kek", "kek"].iter() {
-            assert_eq!(msg, rx.recv().unwrap());
-        }
-    } else {
-        assert!(false, "pool should be create")
+    for &msg in ["lol", "lol", "kek", "kek"].iter() {
+        assert_eq!(msg, rx.recv().unwrap());
     }
 }
 
 #[test]
 fn clone_pool() {
-    if let Ok(pool) = ThreadPool::new(1) {
-        let (tx, rx) = mpsc::sync_channel(1);
+    let (sender, _) = ThreadPool::fixed_size(1);
+    let (tx, rx) = mpsc::sync_channel(1);
 
-        pool.clone().execute(move || {
+    sender
+        .clone()
+        .send(move || {
             tx.send("hey").unwrap();
-        });
+        })
+        .unwrap();
 
-        assert_eq!("hey", rx.recv().unwrap());
-    } else {
-        assert!(false, "pool should be create")
-    }
+    assert_eq!("hey", rx.recv().unwrap());
 }
